21b51d3a4953b3d38aab1cd4734cfbfab4ae20b4
ShukurovOzodbek <shukurov.ozodbek.jaloliddinovich@gmail.com>
2023-12-15 13:06:59 +0500

    simple signup was done

--- main.py
+++ main.py
@@ -1,46 +1,60 @@
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import ReplyKeyboardRemove
 from dotenv import get_variables
 
 from keyboards import contact_keyboard, gender_keyboard
-from utils.checkIsAgeCorrect import check_age
 
 config = get_variables(".env")
 
 bot = Bot(token=config.get('TOKEN'))
+dp = Dispatcher(bot, storage=MemoryStorage())
+dp.middleware.setup(LoggingMiddleware())
 
-dp = Dispatcher(bot)
+
+class RegistrationForm(StatesGroup):
+    waiting_for_contact = State()
+    waiting_for_gender = State()
+    waiting_for_age = State()
 
 
 @dp.message_handler(commands=['start'])
 async def start_bot(message: types.Message):
+    await RegistrationForm.waiting_for_contact.set()
     await message.answer(text='Салам алейкум')
     await message.answer(text='Напишите свой номер телефона', reply_markup=contact_keyboard)
 
 
-@dp.message_handler(content_types=types.ContentType.CONTACT)
-async def handle_contact(message: types.Message):
+@dp.message_handler(content_types=types.ContentType.CONTACT, state=RegistrationForm.waiting_for_contact)
+async def handle_contact(message: types.Message, state: FSMContext):
     contact = message.contact
-    user_id = message.from_user.id
-    phone_number = contact.phone_number
-    first_name = contact.first_name
-    last_name = contact.last_name
+    await state.update_data(contact=contact)
+    await RegistrationForm.next()
     await message.answer(text='Выберите свой пол', reply_markup=gender_keyboard)
 
 
-@dp.message_handler(lambda message: message.text.lower() == 'мужской' or message.text.lower() == 'женский')
-async def handle_age(message: types.Message):
-    await message.answer(text='Сколько вам лет', reply_markup=ReplyKeyboardRemove())
+@dp.message_handler(lambda message: message.text.lower() == 'мужской' or message.text.lower() == 'женский',
+                    state=RegistrationForm.waiting_for_gender)
+async def handle_gender(message: types.Message, state: FSMContext):
+    gender = message.text.lower()
+    await state.update_data(gender=gender)
+    await RegistrationForm.next()
+    await message.answer(text='Сколько вам лет?', reply_markup=ReplyKeyboardRemove())
 
 
-@dp.message_handler(lambda message: message.text.lower() != 'мужской' or message.text.lower() != 'женский')
-async def handle_gender_error(message: types.Message):
-    await message.answer(text='Выберите один из двух вариантов', reply_markup=gender_keyboard)
+@dp.message_handler(lambda message: message.text.isdigit(), state=RegistrationForm.waiting_for_age)
+async def handle_age(message: types.Message, state: FSMContext):
+    age = int(message.text)
+    await state.update_data(age=age)
 
+    user_data = await state.get_data()
 
-@dp.message_handler(check_age)
-async def handle_age(message: types.Message):
-    await message.answer('Отлично')
+    await state.finish()
+
+    await message.answer(text=f'Регистрация завершена. Ваш возраст: {age}.')
 
 
 if __name__ == '__main__':
--- utils/checkIsAgeCorrect.py
+++ utils/checkIsAgeCorrect.py
@@ -7,6 +7,10 @@ async def check_age(message: types.Message):
         age = int(age)
     except ValueError:
         await message.answer(text="Не правильный возраст")
+        return False
 
     if age > 99:
         await message.answer(text="Не правильный возраст")
+        return False
+
+    return True
